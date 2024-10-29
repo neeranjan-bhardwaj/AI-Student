c5147b638e3ab2eedc4e163fd9756e281187d3bd
Neeranjan Bhardwaj <neeranjan5116@gmail.com>
2024-10-29 23:18:41 +0530

    RoadMap&some frontend

--- src/app/AI.js
+++ src/app/AI.js
@@ -2,98 +2,67 @@
 
 import { GoogleGenerativeAI, SchemaType } from '@google/generative-ai'
 
-// let Que="hi"
-let ChatData=[]
-let RoadmapData=[]
 const AI=new GoogleGenerativeAI(process.env.API)
 const Model=AI.getGenerativeModel({ model: "gemini-1.5-flash",tools:[{codeExecution:{}}] })
 
-export async function GetResult (Que) {
-    const Chat=Model.startChat({
-        history:ChatData
-    })
-    let Res=await Chat.sendMessage(Que)
-    ChatData.push({
-        role: "user",
-        parts: [{ text: Que }],
-    },
-    {
-        role: "model",
-        parts: [{ text: Res.response.text() }],
+export async function RoadMap(Topic) {
+    try{
+        if(!Topic)throw Error("Topic is not found") //*check if Topic is there
+        const prompt=``
+        const History=[
+            {
+                role:"user",
+                part:[{text:prompt}]
+            }
+        ]  //* Keep track of History with first prompt
+        const Chat=Model.startChat({  //* Model fine-tuning
+            history:History
+        })
+        const Message=await Chat.sendMessage(Topic) //* send message to model Chat 
+        const Response= await Message.response.text() //* Get response in txt
+
+        History.push(
+            {
+                role:"user",
+                part:[{text:Topic}]
+            },
+            {
+                role:"model",
+                part:[{text:Response}]
+            }) //* making history for chat 
+
+        const cleanedData = Message.replace(/```json/g, "").replace(/```/g, "").trim(); //* trim the response to get only json and replace unwanted staff   
+        const JsonResponse=await JSON.parse(cleanedData)//! Go in front-end Both line
+
+        return Response
+    }catch(err){
+        return err
     }
-    )
-    return Res.response.text()
 }
 
-export async function GetRodmap(Que) {
-    // const Schema={
-    //     type: SchemaType.ARRAY,
-    //     items: {
-    //         type: SchemaType.OBJECT,
-    //         properties: {
-    //         Level: {
-    //             type: SchemaType.STRING,
-                
-    //         },
-    //     },
-    //     required: ["recipeName"],
-    // }
-    // }
-    let History=Model.startChat({
-        history:[
-            {
-                role:"user",
-                parts:[{text:`Act as expart Road map maker and make road map of ${Que}`}]
-            },
-            {
-                role:"model",
-                parts:[{text:`Here is the road map of ${Que}`}]
-            },
-            {
-                role:"user",
-                parts:[{text:`Make it more optimize`}]
-            },
-            {
-                role:"model",
-                parts:[{text:`Here is the more optimize of${Que}`}]
-            },
-            {
-                role:"user",
-                parts:[{text:`Make it more ditle and Level as well and provide Resoures as well  `}]
-            },
-            {
-                role:"model",
-                parts:[{text:`Here is the more ditle road map with Resoures`}]
-            },
-            {
-                role:"user",
-                parts:[{text:`Make this rode map in JSON with out any extra text like json codeLevel = {'Level': string,
-                            'Description':string,
-                            'Learn':Array<Learn>
-                            'Resoures':Array<Resoures>Return: Array<Level>`}]
-            },
-            {
-                role:"model",
-                parts:[{text:`[{'Level':"Phase 1",'Description':"",'Learn':["1. 2. "]'Resoures':[{Books:"",Links:""}]}]`}]
-            }
-            // RoadmapData.length<0?RoadmapData.map((e)=>{e}):{
-            //     role:"model",
-            //     parts:[{text:`Here is the more ditle road map with Resoures`}]
-            // },
-        ]
+export async function Teacher(Text) { //! Can work with PDF need to find way to Check PDF existed or not
+    if(!Text)throw Error("Can not find File")
+    const prompt=``
+    const History=[{
+        role:'user',
+        parts:[{text:prompt}]
+    }]
+    const Chat=Model.startChat({
+        history:History
     })
-    let Res=await History.sendMessage(Que)
-//     RoadmapData.push({
-//         role: "user",
-//         parts: [{ text: Que }],
-//     },
-//     {
-//         role: "model",
-//         parts: [{ text: Res.response.text() }],
-//     },
-// )
+    const Message=await Chat.sendMessage(Text)
+    const Response=await Message.response.text()
 
-    const data=Res.response.text()
-    const cleanedData = data.replace(/```json/g, "").replace(/```/g, "").trim();
-    console.log( JSON.parse(cleanedData))
+    History.push(
+        {
+            role:"user",
+            part:[{text:Response}]
+        },
+        {
+            role:"model",
+            part:[{text:Response}]
+        }
+    )
+
+    return Response
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,44 @@
 "use client"
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Moon, Sun } from 'lucide-react'
+import Link from 'next/link'
 import React, { useState } from 'react'
-import { GetResult,GetRodmap } from './AI'
+
 const page = () => {
-  const [Data,setData]=useState("")
-  const [Result,setResult]=useState()
-  return (
-    <>
-    <nav className='flex text-center'>
-      <select className='w-28 hover:bg-[#D9D9D9] rounded-md drop-shadow-sm ml-5'>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-      </select>
-      <h1 className='w-full text-center text-2xl '>Ai-Student</h1>
-    </nav>
-    <main>
-      <input placeholder='Entry' value={Data} onChange={(e)=>{setData(e.target.value)}} />
-      <button onClick={async()=>{setData("");GetRodmap(Data)}}>Click</button>
-      {Result}
-    </main>
-    </>
-  )
+    const [DarkMode,setDarkMode]=useState(true)
+    return (
+        <div className={DarkMode?"bg-black":null}>
+            <nav className={`flex gap-72 ml-40 mt-4 font-semibold ${DarkMode?'text-white':null}`}>
+                <h1>Zero AI</h1>
+                <div className='flex gap-10'>
+                    <Link href=''>Research</Link>
+                    <Link href=''>About Us</Link>
+                    <Link href=''>Safety</Link>
+                </div>
+                <div className='flex gap-5'>
+                    <Button variant="Ghost" size="icon" onClick={()=>setDarkMode(!DarkMode)}>{DarkMode?<Moon/>:<Sun/>}</Button>
+                    <Avatar>
+                        <AvatarImage src='https://i.pinimg.com/564x/cc/84/e4/cc84e4ceaa62d62ffa9bebcc34193e58.jpg'/>
+                        <AvatarFallback>Tc</AvatarFallback>
+                    </Avatar>
+                </div>
+            </nav>
+            <header className={`flex flex-col items-center font-bold ${DarkMode?'text-white':null}`}>
+                    <h1 className='text-6xl text-center mt-28'>Get Answers. Find RoadMap <br/> Make your Way</h1>
+                    <div className='flex items-center gap-10 mt-10'>
+                        <Button><Link href='/AI'> Fuck AI </Link></Button>
+                        <Button variant="outline">Document</Button>
+                    </div>
+            </header>
+            <main className='flex flex-col items-center'>
+                <h1 className='text-4xl text-center mt-32'>Get proper detail <br/> RoadMap</h1>
+                {/* //! Chang this div to Image or img  */}
+                <div className='w-[60rem] h-[30rem] mt-5  bg-slate-500 '></div>
+            </main>
+        </div>
+    )
 }
 
 export default page
