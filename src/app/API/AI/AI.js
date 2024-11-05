e757437d074487618de76c14665614032b52ea0a
Neeranjan Bhardwaj <neeranjan5116@gmail.com>
2024-11-05 21:58:13 +0530

    almost complete

--- src/app/API/AI/AI.js
+++ src/app/API/AI/AI.js
@@ -9,14 +9,13 @@ const Model=AI.getGenerativeModel({ model: "gemini-1.5-flash",tools:[{codeExecut
 
 export async function RoadMap(Topic) {
     try{
-        connection()
         // if(!Topic)throw Error("Topic is not found") //*check if Topic is there
-        const prompt=``
-        const History=await Schema.find({
-            role: { $exists: true },
-            parts: { $exists: true }
-        }).select("-_id -parts._id").select("-__v");
-        console.log(History.map(e=>e.parts))
+        const prompt=`I want you act as pro of ${Topic}.Now i want you to make a Roadmap of ${Topic}. Make sure the roadmap is in full Detail also It should be const of 3 Level(1.Beginner,2.Advance,3.Pro). and each level have Chapter accordingly(max 10). Each Chapter have All the topic and explain in one line . and also Give one link To the site where he can learn ${Topic} . And also it should be in JSON fermat Like this [{Level:"Beginner",Chapter:[{Topic:"",Explanation:""}],Resource:""},{Level:"Advance",Chapter:[{Topic:"",Explanation:""},Resource:""],},{Level:"Pro",Chapter:[{Topic:"",Explanation:""}],Resource:""}]. I only want JSON Data nothing more `
+
+        const History=[{
+            role:"user",
+            parts:[{text:prompt}]
+        }]
 
         const chat= Model.startChat({  //* Model fine-tuning
             history:History
@@ -25,21 +24,7 @@ export async function RoadMap(Topic) {
         
         const Message=await chat.sendMessage(Topic) //* send message to model Chat 
         const Response= await Message.response.text() //* Get response in txt
-        
-        await Schema.create([
-            {
-                role:"user",
-                parts:[{text:Topic}]
-            },
-            {
-                role:"model",
-                parts:[{text:Response}]
-            }
-        ])
 
-        // const cleanedData = Message.replace(/```json/g, "").replace(/```/g, "").trim(); //* trim the response to get only json and replace unwanted staff   
-        // const JsonResponse=await JSON.parse(cleanedData)//! Go in front-end Both line
-        // console.log(History.map(e=>e.parts.map(e=>e.text)))
         return Response
     }catch(err){
         console.log(err)
--- src/app/page.js
+++ src/app/page.js
@@ -7,25 +7,56 @@ import React, { useState } from 'react'
 import { RoadMap } from './API/AI/AI'
 
 const HomePage=()=>{
-    return <div>this is Home</div>
+    return(
+        <div className='h-full w-full flex flex-col gap-5 justify-center items-center'>
+            <h1 className='font-bold text-4xl '>Start Your Journey</h1>
+            <span className='flex gap-10'>
+                <div className='w-28 h-36 border-2 border-black'></div>
+                <div className='w-28 h-36 border-2 border-black'></div>
+                <div className='w-28 h-36 border-2 border-black'></div>
+                <div className='w-28 h-36 border-2 border-black'></div>
+            </span>
+        </div>
+    )
 }
 
-const ResultPage=()=>{
-    return <div>this is res</div>
+const ResultPage=(Results)=>{
+    return(
+        Results.map(e=>{
+            return(
+                <div className='w-full h-full'>
+                    <h1>{e.Level}</h1>
+                    <ul>
+                        {e.Chapter.map(e=>{return(
+                            <>
+                            <li>{e.Topic}</li>
+                            <p>{e.Explanation}</p>
+                            </>
+                        )})}
+                    </ul>
+                    <Link href={e.Resource}>Resource</Link>
+                </div>
+            )
+        })
+    )
 }
 
 
 const page = () => {
     const [Text,setText]=useState()
-    const [Results,setResults]=useState()
-    const [Loader,setLoader]=useState(false)
+    const [Results,setResults]=useState([])
+    const [Loader,setLoader]=useState(true)
     const [Home,setHome]=useState(true)
     const Generate=async()=>{
         const Chat=Text
         setText("");
         setHome(false)
         setLoader(!Loader)
-        setResults(await RoadMap(Chat))
+        const Data=await RoadMap(Chat)
+        const cleanedData = Data.replace(/```json/g, "").replace(/```/g, "").trim(); //* trim the response to get only json and replace unwanted staff   
+        const JsonResponse=await JSON.parse(cleanedData)
+        setResults(JsonResponse)
+        setLoader(!Loader)
     }
 return (
     <div>
@@ -41,9 +72,9 @@ return (
         </nav>
     </header>
     <main className=' flex flex-col justify-center items-center gap-2 h-[38rem] text-white '>
-        <div className=' h-4/5 w-3/4 '>
-            {Home?<HomePage/>:null}
-            {Loader?"Loading":<ResultPage/>}
+        <div className=' h-5/6 w-full overflow-x-auto '>
+            {/* {Home?HomePage():Loader?"Loading":ResultPage(Results)} */}
+            {ResultPage(Results)}
         </div>
         <div className=' h-12 w-96 flex gap-1 '>
             <Input type='text' placeholder='What you want learn today?' value={Text} onChange={(e)=>{setText(e.target.value)}} className='text-black' />
